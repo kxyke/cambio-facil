467cc5980bce747c546293ad27b0cf0f97518036
Kayke Souza <devkayke@gmail.com>
2026-05-19 23:26:06 -0300

    Retorna a conversão + valor + símbolo da moeda.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+// Cotação de moedas no dia.
+const USD = 4.87
+const EUR = 5.32
+const GBP = 6.08
+
 // Obtendo os elementos do form
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
@@ -12,7 +17,21 @@ amount.addEventListener("input", () => {
 form.onsubmit = (event) => {
   event.preventDefault()
 
-  console.log(currency.value)
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$")
+      break
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€")
+      break
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£")
+      break
+  }
 }
 
 // Funcão para converter a moeda.
+function convertCurrency(amount, prince, symbol){
+  console.log(amount, prince, symbol)
+
+}
